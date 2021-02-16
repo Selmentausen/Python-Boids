f98c598974ffccb5e04f137ffa14917af5039166
Selmentausen <selmentausen@gmail.com>
2021-02-16 18:51:18 +0300

    Did some refactoring by creating base class Shape which are inherited by class Cuboid and Pyramid. Created the class Pyramid that can generate 3d Pyramid objects. And made the draw_shape function a method of Shape class

--- main.py
+++ main.py
@@ -6,10 +6,57 @@ screen = pygame.display.set_mode((400, 400))
 running = True
 
 
-class Cuboid:
-    def __init__(self, coords, dimensions):
-        x, y, z = coords
-        w, h, d = dimensions
+class Shape:
+    def __init__(self, coord, dimensions, node_color=(0, 0, 255), edge_color=(0, 255, 0), node_size=10):
+        self.coord = coord
+        self.dimensions = dimensions
+        self.nodes = []
+        self.edges = []
+        self.node_color = node_color
+        self.edge_color = edge_color
+        self.node_size = node_size
+        self.center = coord
+
+    def draw(self, surface):
+        for edge in self.edges:
+            start_node, end_node = self.nodes[edge[0]], self.nodes[edge[1]]
+            pygame.draw.line(surface, self.edge_color,
+                             (start_node[0] + self.node_size // 2, start_node[1] + self.node_size // 2),
+                             (end_node[0] + self.node_size // 2, end_node[1] + self.node_size // 2))
+        for node in self.nodes:
+            pygame.draw.ellipse(surface, self.node_color, (node[0], node[1], self.node_size, self.node_size))
+
+
+class Pyramid(Shape):
+    def __init__(self, coord, dimensions, node_color=(0, 0, 255), edge_color=(0, 255, 0), node_size=10):
+        super(Pyramid, self).__init__(coord, dimensions, node_color, edge_color, node_size)
+        x, y, z = self.coord
+        w, h, d = self.dimensions
+        self.nodes = [
+            [x, y, z],
+            [x + w, y, z],
+            [x, y + h, z],
+            [x + w, y + h, z],
+            [x + w // 2, y + h // 2, z + d]
+        ]
+        self.edges = [
+            [0, 1],
+            [0, 2],
+            [1, 3],
+            [2, 3],
+            [0, 4],
+            [1, 4],
+            [2, 4],
+            [3, 4]
+        ]
+        self.center = (x + w // 2, y + h // 2, z + d // 2)
+
+
+class Cuboid(Shape):
+    def __init__(self, coord, dimensions, node_color=(0, 0, 255), edge_color=(0, 255, 0), node_size=10):
+        super(Cuboid, self).__init__(coord, dimensions, node_color, edge_color, node_size)
+        x, y, z = self.coord
+        w, h, d = self.dimensions
         self.nodes = [
             [x, y, z],
             [x, y, z + d],
@@ -28,16 +75,7 @@ class Cuboid:
         self.center = (x + w // 2, y + h // 2, z + d // 2)
 
 
-def draw_shape(surface, nodes, edges, node_color=(34, 68, 204), edge_color=(40, 168, 107), node_size=10):
-    for edge in edges:
-        start_node, end_node = nodes[edge[0]], nodes[edge[1]]
-        pygame.draw.line(surface, node_color, (start_node[0] + node_size // 2, start_node[1] + node_size // 2),
-                         (end_node[0] + node_size // 2, end_node[1] + node_size // 2))
-    for node in nodes:
-        pygame.draw.ellipse(surface, edge_color, (node[0], node[1], node_size, node_size))
-
-
-def rotateZ_shape(degrees, nodes, rotation_center):
+def rotate_z_shape(degrees, nodes, rotation_center):
     sin_theta = math.sin(math.radians(degrees))
     cos_theta = math.cos(math.radians(degrees))
     for node in nodes:
@@ -46,7 +84,7 @@ def rotateZ_shape(degrees, nodes, rotation_center):
         node[1] = rotation_center[1] + (y * cos_theta + x * sin_theta)
 
 
-def rotateX_shape(degrees, nodes, rotation_center):
+def rotate_x_shape(degrees, nodes, rotation_center):
     sin_theta = math.sin(math.radians(degrees))
     cos_theta = math.cos(math.radians(degrees))
     for node in nodes:
@@ -55,7 +93,7 @@ def rotateX_shape(degrees, nodes, rotation_center):
         node[2] = rotation_center[2] + (z * cos_theta + y * sin_theta)
 
 
-def rotateY_shape(degrees, nodes, rotation_center):
+def rotate_y_shape(degrees, nodes, rotation_center):
     sin_theta = math.sin(math.radians(degrees))
     cos_theta = math.cos(math.radians(degrees))
     for node in nodes:
@@ -65,9 +103,7 @@ def rotateY_shape(degrees, nodes, rotation_center):
 
 
 cube = Cuboid((100, 100, 100), (200, 200, 200))
-rotateY_shape(50, cube.nodes, cube.center)
-rotateX_shape(15, cube.nodes, cube.center)
-# rotateZ_shape(60, cube_nodes, cube_center)
+cone = Pyramid((100, 100, 100), (200, 200, 200))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -77,10 +113,14 @@ while running:
                 running = False
         if event.type == pygame.MOUSEMOTION:
             if pygame.mouse.get_pressed(3)[0]:
-                rotateY_shape(event.rel[0], cube.nodes, cube.center)
-                rotateX_shape(event.rel[1], cube.nodes, cube.center)
+                pass
+                rotate_y_shape(event.rel[0], cone.nodes, (200, 200, 200))
+                rotate_x_shape(event.rel[1], cone.nodes, (200, 200, 200))
+                rotate_y_shape(event.rel[0], cube.nodes, cube.center)
+                rotate_x_shape(event.rel[1], cube.nodes, cube.center)
 
     screen.fill(pygame.Color('white'))
-    draw_shape(screen, cube.nodes, cube.edges)
+    cone.draw(screen)
+    cube.draw(screen)
     pygame.display.flip()
 pygame.quit()
