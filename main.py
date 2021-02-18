8e140d1b770959db5d8f45e2966cf286d074a5c9
Ibrahim <Selmentausen@gmail.com>
2021-02-18 23:11:59 +0300

    Added time and ability to move Boid forward based on rotation

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import pygame
 from shapes_3d import Cuboid, Pyramid
 from shapes_2d import Boid2D
 
+
 pygame.init()
+clock = pygame.time.Clock()
+FPS = 60
 screen = pygame.display.set_mode((400, 400))
 running = True
 boid = Boid2D((100, 100), 50, 50)
 # cube = Cuboid((100, 100, 100), (200, 200, 200))
 # cone = Pyramid((100, 100, 100), (200, 200, 200), edge_color=(255, 0, 0), node_color=(0, 0, 0))
+move = False
 
 while running:
     for event in pygame.event.get():
@@ -18,12 +22,20 @@ while running:
                 running = False
             if event.key == pygame.K_r:
                 boid.set_rotation(0)
+            if event.key == pygame.K_w:
+                move = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                move = False
         if event.type == pygame.MOUSEMOTION:
             if pygame.mouse.get_pressed(3)[0]:
                 boid.rotate(-event.rel[0])
     screen.fill(pygame.Color('white'))
+    if move:
+        boid.move_forward(clock.get_time() / 1000)
     boid.draw(screen)
 #     cube.draw(screen)
 #     cone.draw(screen)
     pygame.display.flip()
+    clock.tick(60)
 pygame.quit()
--- shapes_2d.py
+++ shapes_2d.py
@@ -9,10 +9,12 @@ class Boid2D:
         self.height = height
         self.color = (0, 0, 255)
         self.nodes = [[self.x, self.y],
-                      [self.x + width, self.y],
-                      [self.x + width // 2, self.y + height]]
+                      [self.x, self.y + height],
+                      [self.x + width, self.y + height // 2]]
+        self.forward_node = self.nodes[-1]
         self.center = [sum(n[0] for n in self.nodes) // 3, sum(n[1] for n in self.nodes) // 3]
         self.rotation = 0
+        self.speed = 100
 
     def draw(self, surface):
         pygame.draw.polygon(surface, self.color, self.nodes)
@@ -28,5 +30,18 @@ class Boid2D:
 
     def set_rotation(self, degrees):
         self.rotate(degrees % 360 - self.rotation)
+
+    def update_center(self):
+        self.center = [sum(n[0] for n in self.nodes) // 3, sum(n[1] for n in self.nodes) // 3]
+
+    def move_forward(self, delta_t):
+        unit_x = self.speed * cos(radians(self.rotation))
+        unit_y = self.speed * sin(radians(self.rotation))
+        for node in self.nodes:
+            node[0] += unit_x * delta_t
+            node[1] += unit_y * delta_t
+        self.update_center()
+
+
 
 
